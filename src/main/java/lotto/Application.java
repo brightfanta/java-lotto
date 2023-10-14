37df2f6647ca81f24e438851672fc9c3de78571c
brightfanta <brightfanta@gmail.com>
2023-10-14 15:51:28 +0900

    feat: add function to generate lottos and lotto numbers

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Application {
@@ -13,15 +14,40 @@ public class Application {
         String bonusNum;
         Integer bonusNumber;
         List<Integer> winningNumbers = new ArrayList<>();
+        Integer lotteryCnt = 0;
 
         //당첨번호 입력받기
         getWinnigNumbers(winningNumbers);
-        System.out.println("winningNumbers is " + winningNumbers);
-
+        /*System.out.println("winningNumbers is " + winningNumbers);//전체 출력 시 toString() 메서드 필요 없음
+        for (Integer winningNumber : winningNumbers) {
+            System.out.println("winningNumber = " + winningNumber);
+        }*/
 
         //보너스번호 입력받기
         bonusNumber = getBonusNumber();
-        System.out.println("bonusNumber = " + bonusNumber);
+        /*System.out.println("bonusNumber = " + bonusNumber);*/
+
+//        lotteryCnt = 8;
+        //로또 생성
+        Lotto[] lottos = new Lotto[lotteryCnt];
+        generateLottos(lotteryCnt, lottos);
+
+        for (Lotto lotto : lottos) {
+            System.out.println("lotto = " + lotto.getNumbers());
+        }
+    }
+
+    private static void generateLottos(Integer lotteryCnt, Lotto[] lottos) {
+        for (int i = 0; i < lotteryCnt; i++) {
+            //로또에 입력할 1~45 숫자 6개 성성
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            //6개의 숫자 오름차순 정렬
+            numbers.sort(Comparator.naturalOrder());
+
+            //오름차순으로 정렬된 숫자로 Lotto 객체에 입력
+            lottos[i] = new Lotto(numbers);
+        }
     }
 
     private static Integer getBonusNumber() {

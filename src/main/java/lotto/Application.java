295becae917291a3da05033fd5a8c1cda46765e7
brightfanta <brightfanta@gmail.com>
2023-10-15 21:48:50 +0900

    refactor: delete print method to debug feat: add function to handle exceptions

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -17,15 +17,21 @@ public class Application {
         List<Integer> matchingNumCnt = new ArrayList<>();
         String budgetInput;
         Integer budget;
+        double earningsPerBudgetRound = 0.0;
+        boolean exceptionCheck = true;
 
         //로또 구입금액 입력
         System.out.println("구입금액을 입력해 주세요.");
         budgetInput = Console.readLine();
         System.out.println();
         budget = Integer.parseInt(budgetInput);
-        lotteryCnt = budget / 1000;
 
-        //로또 생성
+        //예외처리(1000으로 나누어 떨어지지 않은 금액 입력 시)
+        exceptionCheck = InputBudgetExceptionCheck(budget);
+        budgetException(exceptionCheck);
+
+        //구입 금액으로 로또 개수 계산 및 로또 생성
+        lotteryCnt = budget / 1000;
         System.out.println(lotteryCnt + "개를 구매했습니다.");
         Lotto[] lotteries = new Lotto[lotteryCnt];
         generateLottos(lotteryCnt, lotteries);
@@ -38,33 +44,63 @@ public class Application {
             System.out.println("winningNumber = " + winningNumber);
         }*/
 
+        //예외처리(1~45의 범위를 벗어난 당첨번호)
+        exceptionCheck = InputWinningNumbersExceptionCheck(winningNumbers);
+        numbersException(exceptionCheck);
+
         //보너스번호 입력받기
         System.out.println("보너스 번호를 입력해 주세요.");
         bonusNumber = getBonusNumber();
         /*System.out.println("bonusNumber = " + bonusNumber);*/
 
+        //예외처리(1~45의 범위를 벗어난 보너스번호)
+        exceptionCheck = InputBonusNumberExceptionCheck(bonusNumber);
+        numbersException(exceptionCheck);
 
         //로또와 당첨 번호 비교
         getMatchingCnt(winningNumbers, matchingNumCnt, lotteries);
-        System.out.println("matchingNumCnt = " + matchingNumCnt);
+//        System.out.println("matchingNumCnt = " + matchingNumCnt);
 
         //로또와 보너스 번호 비교 후 2, 3등 산출
-        for (Integer matchCnt : matchingNumCnt) {
-            int bonusIdx = 0;
-            if (matchCnt == 5) {
-                bonusIdx = matchingNumCnt.indexOf(matchCnt);
-                bonusNumMatchingCnt(bonusNumber, matchingNumCnt, lotteries);
-            }
-        }
-        System.out.println();
+        getSecondNThirdRank(bonusNumber, matchingNumCnt, lotteries);
 
         //당첨통계
         System.out.println("당첨 통계");
         System.out.println("---");
 
+        //당첨금액 ,로 자리수 표기
         DecimalFormat df = new DecimalFormat("###,###");
 
+        //수익률 계산
+        earningsPerBudgetRound = getEarningsPerBudget(matchingNumCnt, budget);
 
+        //통계 출력
+        printStatistics(matchingNumCnt, df, earningsPerBudgetRound);
+
+    }
+
+    private static void getSecondNThirdRank(Integer bonusNumber, List<Integer> matchingNumCnt, Lotto[] lotteries) {
+        for (Integer matchCnt : matchingNumCnt) {
+            if (matchCnt == 5) {
+                bonusNumMatchingCnt(bonusNumber, matchingNumCnt, lotteries);
+            }
+        }
+    }
+
+    private static double getEarningsPerBudget(List<Integer> matchingNumCnt, Integer budget) {
+        Integer earnings = 0;
+        int idxTemp = 0;
+
+        for (Rank rank : Rank.values()) {
+
+            earnings += rank.totalPrize(Collections.frequency(matchingNumCnt, Integer.valueOf(idxTemp + 3)));
+            idxTemp++;
+        }
+
+        return (double)(earnings)/ budget * 100.0;
+    }
+
+    private static void printStatistics(List<Integer> matchingNumCnt, DecimalFormat df, double earningsPerBudgetRound) {
         for (Rank rank : Rank.values()) {
 
             if (rank.id < 3) {
@@ -72,38 +108,24 @@ public class Application {
                 System.out.println((rank.id + 3) + "개 일치 (" + df.format(rank.prize) + "원) - " + Collections.frequency(matchingNumCnt, (rank.id + 3)) + "개");
             }
 
-            if (rank.id == 3) {
-
-                System.out.println((rank.id + 2) + "개 일치, 보너스볼 일치 (" + df.format(rank.prize) + "원) - " + Collections.frequency(matchingNumCnt, 7) + "개");
-            }
-
             if (rank.id > 3) {
 
-                System.out.println((rank.id + 2) + "개 일치 (" + df.format(rank.prize) + "원) - " + Collections.frequency(matchingNumCnt, (rank.id + 2)) + "개");
+                System.out.println((rank.id + 2) + "개 일치 (" + df.format(rank.prize) + "원) - " + Collections.frequency(matchingNumCnt, (rank.id + 3)) + "개");
+            }
+
+            if (rank.id == 3) {
+
+                System.out.println((rank.id + 2) + "개 일치, 보너스 볼 일치 (" + df.format(rank.prize) + "원) - " + Collections.frequency(matchingNumCnt, (rank.id + 3)) + "개");
             }
         }
 
-        double earningsPerBudget = 0;
-
-        Integer earnings = 0;
-        for (Rank rank : Rank.values()) {
-            int idxTemp = 0;
-            earnings += rank.totalPrize(Collections.frequency(matchingNumCnt, idxTemp + 3));
-            idxTemp++;
-        }
-
-        earningsPerBudget = (earnings)/budget;
-// earningsPerBudget += ();
-        //반복문 종료 후 나눗셈해서 수익률 계산하기
-
-
+        System.out.println("총 수익률은 " + String.format("%.1f", earningsPerBudgetRound) + "%입니다.");
     }
 
     private static void getMatchingCnt(List<Integer> winningNumbers, List<Integer> matchingNumCnt, Lotto[] lotteries) {
 
-
         for (Lotto lotto : lotteries) {
-            System.out.println("lotto = " + lotto.getNumbers());
+//            System.out.println("lotto = " + lotto.getNumbers());
 //            System.out.println("winningNumbers = " + winningNumbers);
 
 //            matchingNumsTemp = winningNumbers; // 주소를 참조하는구나 값 자체가 아니라
@@ -122,7 +144,6 @@ public class Application {
             }
             //matchingNumCnt는 0~5, 7의 값을 갖는다.
         }
-        System.out.println("matchingNumCnt = " + matchingNumCnt);
     }
 
     private static void bonusNumMatchingCnt(Integer bonusNumber, List<Integer> matchingNumCnt, Lotto[] lotteries) {
@@ -131,7 +152,7 @@ public class Application {
 
         for (Lotto lottery : lotteries) {
             if (lottery.getNumbers().contains(bonusNumber) && matchingNumCnt.get(listCntTemp) == 5) {
-                matchingNumCnt.set(listCntTemp, 7);
+                matchingNumCnt.set(listCntTemp, 6);
             }
             listCntTemp++;
         }
@@ -140,10 +161,10 @@ public class Application {
     private static void generateLottos(Integer lotteryCnt, Lotto[] lottos) {
         for (int i = 0; i < lotteryCnt; i++) {
             //로또에 입력할 1~45 숫자 6개 성성
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
 
             //6개의 숫자 오름차순 정렬
-            numbers.sort(Comparator.naturalOrder());
+            numbers.sort(null);
 
             //오름차순으로 정렬된 숫자로 Lotto 객체에 입력
             lottos[i] = new Lotto(numbers);
@@ -165,6 +186,7 @@ public class Application {
         for (int i = 0; i < 6; i++) {
 
             winningNumbers.add(Integer.parseInt(winningNum.split(",")[i]));
+            winningNumbers.sort(Comparator.naturalOrder());
         }
         System.out.println();
     }
@@ -213,13 +235,42 @@ public class Application {
 
         abstract int totalPrize(int lottoMatchedCnt);
 
-        public int getPrize() {
-            return prize;
-        }
+    }
 
-        public int getId() {
-            return id;
+    private static void budgetException(boolean priceInputCheck) throws IllegalArgumentException {
+        if (!priceInputCheck) {
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지지 않는 금액입니다.");
         }
     }
 
+    private static void numbersException(boolean numbersInputCheck) throws IllegalArgumentException {
+        if (!numbersInputCheck) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private static boolean InputBudgetExceptionCheck(Integer budget) {
+        if (budget % 1000 != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean InputWinningNumbersExceptionCheck(List<Integer> winningNumbers) {
+        for (Integer winningNumber : winningNumbers) {
+            if (winningNumber < 1 || winningNumber > 45) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean InputBonusNumberExceptionCheck(Integer bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Comparator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
